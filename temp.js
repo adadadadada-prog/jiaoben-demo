0c97cb325c0fe4c9d0e99d11feb5d47b6627f36c
zdw <15525928991@163.com>
2024-09-10 22:14:42 +0800

    creat temp.js

---
+++ temp.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://tampermonkey.net/
+// @version      2024-09-10
+// @description  try to take over the world!
+// @author       zdw
+// @match        *://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('hello world');
+    
+})();
